f3b17fa7c2b9a3b39a508ca4331d50197b163877
tawehbeysolow-pawn <taweh.beysolowii@gmail.com>
2021-05-04 23:14:43 -0700

    removing package-lock

--- contracts/utils/LoanNoteUtilities.sol
+++ contracts/utils/LoanNoteUtilities.sol
@@ -7,9 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "../interfaces/ILoanCore.sol";
 import "./LoanMetadata.sol";
 
-/** 
 contract LoanNoteUtilities is Context, ERC721, ERC721Enumerable, ERC721Pausable {
-    
     address public loanCore;
 
     constructor() public ERC721() {}
@@ -22,20 +20,30 @@ contract LoanNoteUtilities is Context, ERC721, ERC721Enumerable, ERC721Pausable 
         return status == LoanMetadata.Status.OPEN || status == LoanMetadata.Status.DEFAULT;
     }
 
- 
+    /**
+     * @dev See the current status of the loan this note is attached to.
+     *
+     * This is a convenienc function that gives a wallet or contract interacting
+     * the ability
+     */
     function checkStatus(uint256 tokenId) public view returns (LoanMetadata.Status status) {
         require(_exists(tokenId), "LenderNote: loan does not exist");
 
         return ILoanCore(loanCore).getLoanByLenderNote(tokenId).status;
     }
 
- 
+    /**
+     * @dev See the current status of the loan this note is attached to.
+     */
     function checkTerms(uint256 tokenId) public view returns (LoanMetadata.Terms memory terms) {
         require(_exists(tokenId), "LenderNote: loan does not exist");
 
         return ILoanCore(loanCore).getLoanByLenderNote(tokenId).terms;
     }
 
+    /**
+     * @dev Hook that is called before any token transfer
+     */
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -44,7 +52,9 @@ contract LoanNoteUtilities is Context, ERC721, ERC721Enumerable, ERC721Pausable 
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -55,4 +65,3 @@ contract LoanNoteUtilities is Context, ERC721, ERC721Enumerable, ERC721Pausable 
         return super.supportsInterface(interfaceId);
     }
 }
-*/
