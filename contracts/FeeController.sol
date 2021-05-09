644d49dd81333f673ad59d329e1c269a5b5d0896
davidkuchar <davidkuchar@gmail.com>
2021-05-09 11:57:06 -0700

    fix(feecontroller): fixes linter errors for feecontroller and ifeecontroller

--- contracts/FeeController.sol
+++ contracts/FeeController.sol
@@ -1,21 +1,19 @@
 pragma solidity ^0.8.0;
 
 /**
-Fee Controller is intended to be an upgradable component of Pawnfi
-where new fees can be added or modified based on different user attributes 
+ * Fee Controller is intended to be an upgradable component of Pawnfi
+ * where new fees can be added or modified based on different user attributes
+ *
+ * Type/size of loan being requested
+ * Amount of tokens being staked, etc
+ *
+ * Version 1 (as of 4/21/2021) Capabilities:
+ *
+ * FeeController will be called once after a loan has been matched so that we can
+ * create an origination fee (2% credited to PawnFi)
+ * @dev support for floating point originationFee should be discussed
+ */
 
-Type/size of loan being requested 
-Amount of tokens being staked, etc
-
-Version 1 (as of 4/21/2021) Capabilities: 
-
-FeeController will be called once after a loan has been matched so that we can
-create an origination fee (2% credited to PawnFi) 
-
-* @dev support for floating point originationFee should be discussed
-
-
-*/
 contract FeeController {
     uint256 public originationFee;
 
@@ -32,6 +30,7 @@ contract FeeController {
     * - address must be a valid ERC20  contract implementing balanceOf
 
     */
+
     function getFeeOfType(string memory loanType) public returns (uint256) {
         if (keccak256(bytes(loanType)) == keccak256(bytes("originationFee"))) {
             //Use DS Math code to return this : originationFee = amount * .02;
