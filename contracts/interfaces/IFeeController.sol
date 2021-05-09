ed2627f8890c66a3448252974a3c9ae0cccc0a8d
tawehbeysolow-pawn <taweh.beysolowii@gmail.com>
2021-05-09 10:01:25 -0700

    adding comments

---
+++ contracts/interfaces/IFeeController.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+/**
+* @dev Interface for a FeeController contract
+*/
+interface FeeController { 
+
+    /**
+    * @dev Emitted when a Loan is created 
+    */
+    function getOriginationFee(uint256 amount) external returns(uint256);
+
+}
