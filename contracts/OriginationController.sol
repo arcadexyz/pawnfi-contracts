46c86caefbe0d86451cdb5c53d6394f4715df0c8
tawehbeysolow-pawn <taweh@pawn.finance>
2021-06-15 22:40:10 -0700

    fix(originationcontroller): address comments

--- contracts/OriginationController.sol
+++ contracts/OriginationController.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
@@ -16,10 +15,8 @@ import "./interfaces/IERC721Permit.sol";
 
 /**
  * @dev
- *
  * This contract uses {AccessControl} to lock permissioned functions using the
  * different roles - head to its documentation for details.
- *
  */
 contract OriginationController is Context, AccessControlEnumerable, IOriginationController {
     uint256 public loanId;
@@ -29,27 +26,24 @@ contract OriginationController is Context, AccessControlEnumerable, IOrigination
 
     /**
      * @dev
-     * Grants `DEFAULT_ADMIN_ROLE` to the account that deploys the contract. Admins
      * can pause the contract if needed.
-     *
      */
     constructor(address _loanCore, address _assetWrapper) {
         require(_loanCore != address(0), "loanCore address must be defined");
         loanCore = _loanCore;
         assetWrapper = _assetWrapper;
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
     /**
      * @dev initializes loan from loan core
-     *
      * Requirements:
-     *
-     * - The caller must be a borrower or lender.
+     * - The caller must be a borrower or lender
      * - The external signer must not be msg.sender
      * - The external signer must be a borrower or lender
-     *
-     *
+     * loanTerms - struct containing specifics of loan made between lender and borrower
+     * borrower - address of borrowerPromissory note 
+     * lender - address of lenderPromissory note
+     * v, r, s - signature from erc20
      */
     function initializeLoan(
         LoanData.LoanTerms calldata loanTerms,
@@ -58,10 +52,10 @@ contract OriginationController is Context, AccessControlEnumerable, IOrigination
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) external override {
-        require(msg.sender == lender || msg.sender == borrower, "msg.sender must be lender or borrower");
+    ) public override {
+        require(_msgSender() == lender || _msgSender() == borrower, "Origination: sender not participant");
 
-        bytes32 _loanTerms =
+        bytes32 loanHash =
             keccak256(
                 abi.encodePacked(
                     loanTerms.dueDate,
@@ -72,31 +66,38 @@ contract OriginationController is Context, AccessControlEnumerable, IOrigination
                 )
             );
 
-        address externalSigner = _loanTerms.toEthSignedMessageHash().recover(v, r, s);
+        address externalSigner = loanHash.toEthSignedMessageHash().recover(v, r, s);
 
-        require(externalSigner == lender || externalSigner == borrower, "external signer must be borrower or lender");
+        require(externalSigner == lender && externalSigner != _msgSender()
+        || externalSigner == borrower && externalSigner != _msgSender(), 
+        "external signer must be borrower or lender");
 
         require(
-            IERC721(lender).getApproved(loanTerms.collateralTokenId) != address(0),
+            IERC721(borrower).getApproved(loanTerms.collateralTokenId) != address(0),
             "must be approved to accept collateral token"
         );
 
         require(
-            IERC20(loanTerms.payableCurrency).allowance(address(lender), address(this)) > 0,
+            IERC20(loanTerms.payableCurrency).allowance(address(lender), address(this)) >= loanTerms.principal,
             "must be approved to accept funding currency"
         );
 
-        TransferHelper.safeTransferFrom(loanTerms.payableCurrency, lender, borrower, loanTerms.principal);
+        TransferHelper.safeTransferFrom(loanTerms.payableCurrency, lender, loanCore, loanTerms.principal);
         IERC721(address(this)).transferFrom(borrower, loanCore, loanTerms.collateralTokenId);
 
-        ILoanCore(loanCore).createLoan(loanTerms);
+        loanId = ILoanCore(loanCore).createLoan(loanTerms);
         ILoanCore(loanCore).startLoan(lender, borrower, loanId);
     }
 
+
     /**
-     * @dev initializes loan from loan core with Collateral Permit
-     *
-     **/
+     * @dev creates a new loan, with permit attached
+     * loanTerms - struct containing specifics of loan made between lender and borrower
+     * borrower - address of borrowerPromissory note 
+     * lender - address of lenderPromissory note
+     * v, r, s - signature from erc20
+     * collateralV, collateralR, collateralS - signature from collateral
+     */
     function initializeLoanWithCollateralPermit(
         LoanData.LoanTerms calldata loanTerms,
         address borrower,
@@ -112,12 +113,12 @@ contract OriginationController is Context, AccessControlEnumerable, IOrigination
             borrower,
             address(this),
             loanTerms.collateralTokenId,
-            block.timestamp + 1000,
+            block.timestamp,
             collateralV,
             collateralR,
             collateralS
         );
 
-        this.initializeLoan(loanTerms, borrower, lender, v, r, s);
+        initializeLoan(loanTerms, borrower, lender, v, r, s);
     }
 }
