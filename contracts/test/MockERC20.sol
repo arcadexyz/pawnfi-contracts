0a6888ddfbd586c631558dc43ea4534163129fdf
Kevin Kennis <kevinvkennis@gmail.com>
2021-08-20 12:07:28 -0400

    fix(erc20): change one token to 6 decinals

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract MockERC20 is Context, ERC20Burnable {
     /**
@@ -17,5 +17,20 @@ contract MockERC20 is Context, ERC20Burnable {
      */
     function mint(address to, uint256 amount) public virtual {
         _mint(to, amount);
+    }
+}
+
+contract MockERC20WithDecimals is ERC20PresetMinterPauser {
+    uint8 private _decimals;
+
+    /**
+     * @dev Initializes ERC20 token
+     */
+    constructor(string memory name, string memory symbol, uint8 decimals) ERC20PresetMinterPauser(name, symbol) {
+        _decimals = decimals;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
     }
 }
