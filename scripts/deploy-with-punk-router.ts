26e66b47062ef791c9e6f152f5083e24b31f7232
John Terzis <john.c.terzis@gmail.com>
2021-08-25 13:31:16 -0700

    lint fixes

--- scripts/deploy-with-punk-router.ts
+++ scripts/deploy-with-punk-router.ts
@@ -1,10 +1,10 @@
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-import { main as deployMain, DeployedResources } from './deploy';
+import { main as deployMain, DeployedResources } from "./deploy";
 
 export interface DeployedResourcesWithPunks extends DeployedResources {
-  punkRouter: Contract
+  punkRouter: Contract;
 }
 
 export async function main(
@@ -13,32 +13,32 @@ export async function main(
   WRAPPED_PUNKS = "0xb7F7F6C52F2e2fdb1963Eab30438024864c313F6",
   CRYPTO_PUNKS = "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
 ): Promise<DeployedResourcesWithPunks> {
-    const {
-      assetWrapper,
-      feeController,
-      loanCore,
-      borrowerNote,
-      lenderNote,
-      repaymentController,
-      originationController,
-    } = await deployMain(ORIGINATOR_ROLE, REPAYER_ROLE);
+  const {
+    assetWrapper,
+    feeController,
+    loanCore,
+    borrowerNote,
+    lenderNote,
+    repaymentController,
+    originationController,
+  } = await deployMain(ORIGINATOR_ROLE, REPAYER_ROLE);
 
-    const PunkRouter = await ethers.getContractFactor("PunkRouter");
-    const punkRouter = await PunkRouter.deploy(assetWrapper.address, WRAPPED_PUNKS, CRYPTO_PUNKS);
-    await punkRouter.deployed();
+  const PunkRouter = await ethers.getContractFactor("PunkRouter");
+  const punkRouter = await PunkRouter.deploy(assetWrapper.address, WRAPPED_PUNKS, CRYPTO_PUNKS);
+  await punkRouter.deployed();
 
-    console.log("PunkRouter deployed to:", punkRouter.address);
+  console.log("PunkRouter deployed to:", punkRouter.address);
 
-    return {
-      assetWrapper,
-      feeController,
-      loanCore,
-      borrowerNote,
-      lenderNote,
-      repaymentController,
-      originationController,
-      punkRouter,
-    };
+  return {
+    assetWrapper,
+    feeController,
+    loanCore,
+    borrowerNote,
+    lenderNote,
+    repaymentController,
+    originationController,
+    punkRouter,
+  };
 }
 
 // We recommend this pattern to be able to use async/await everywhere
